5fafbe7c8c3268bad3b831df01cfd7d4730cd5a2
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2025-01-08 00:05:02 +0200

    small change

--- src/components/MarkdownGenerator.tsx
+++ src/components/MarkdownGenerator.tsx
@@ -151,7 +151,7 @@ export default function MarkdownGenerator() {
               disabled={!isFormValid()} // Disable the button if the form is invalid
               className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              Generate Markdown
+              Generate Markdown.
             </button>
           </div>
         </div>
